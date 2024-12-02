3a0c4c83d37381a4a890be1ef3a327143647f1bb
Sincan Maulana <sincanmaulanaa@gmail.com>
2024-12-02 21:08:13 +0700

    Refactor import path for Apollo Client in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
-import client from '../../lib/apolloClient';
+import client from '@/lib/apolloClient';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
