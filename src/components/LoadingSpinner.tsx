82ecfe9b307fbe2e1aeff83ed44ec50178acba14
Sincan Maulana <sincanmaulanaa@gmail.com>
2024-12-02 21:06:40 +0700

    Remove unused Apollo Client and related components

--- src/components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className='flex justify-center items-center h-screen bg-black'>
-      <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-yellow-300'></div>
-    </div>
-  );
-}
