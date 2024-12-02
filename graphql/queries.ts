82b5c991adf765ad5307470f3743335c04b0f16b
Sincan Maulana <sincanmaulanaa@gmail.com>
2024-12-02 21:06:48 +0700

    Add GraphQL queries for character and movie data retrieval

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -52,3 +52,77 @@ export const ALL_CHARACTERS = gql`
     }
   }
 `;
+
+export const GET_CHARACTER = gql`
+  query GetCharacter($id: ID!) {
+    person(id: $id) {
+      id
+      name
+      birthYear
+      gender
+      height
+      mass
+      skinColor
+      eyeColor
+      hairColor
+      species {
+        name
+      }
+      homeworld {
+        name
+      }
+      filmConnection {
+        films {
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const ALL_MOVIES = gql`
+  query AllMovies($first: Int, $after: String) {
+    allFilms(first: $first, after: $after) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          id
+          title
+          episodeID
+          releaseDate
+          director
+          producers
+        }
+      }
+    }
+  }
+`;
+
+export const GET_MOVIE = gql`
+  query GetMovie($id: ID!) {
+    film(id: $id) {
+      id
+      title
+      episodeID
+      openingCrawl
+      director
+      producers
+      releaseDate
+      characterConnection {
+        characters {
+          id
+          name
+        }
+      }
+      planetConnection {
+        planets {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
